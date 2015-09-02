1f5984924b6baa30d41c0ed006ba7de672a9fbd1
David Robertson <theonesuperdave@yahoo.com>
2015-09-02 15:39:42 -0500

    Added vm for nav so I can control it my way

---
+++ rivetsJSConDurandalJS/App/viewmodels/nav.js
@@ -0,0 +1,22 @@
+﻿define(['durandal/app', 'plugins/router'], function (app, router) {
+    var nav =
+    {
+        title: app.title,
+        router: router,
+        activate: function () {
+        },
+        binding: function () {
+            nav.rivetsBinding = rivets.bind($('#nav'), { vm: nav });
+        },
+        compositionComplete: function () {
+        },
+        deactivated: function () {
+            nav.rivetsBinding.unbind();
+        },
+        detached: function () {
+            nav.rivetsBinding.unbind();
+        }
+    }
+
+    return nav;
+});
