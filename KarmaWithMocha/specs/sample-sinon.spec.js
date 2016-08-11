8cb4b4a48f575b73b279829960cc8e7f587aef97
Dave Robertson <theonesuperdave@yahoo.com>
2016-08-11 14:29:14 -0500

    added sinon spy

--- KarmaWithMocha/specs/sample-sinon.spec.js
+++ KarmaWithMocha/specs/sample-sinon.spec.js
@@ -24,7 +24,7 @@
                 callbackScoreGoal.calledOn(target).should.be.true();
 
                 callbackGoalCount.called.should.be.false();
-                
+
                 target.scoreGoal.restore();
                 target.goalCount.restore();
             });
