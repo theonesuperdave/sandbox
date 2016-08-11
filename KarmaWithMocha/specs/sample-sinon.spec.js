0853f824c1e8b5118bc18a79863d2091cf198379
Dave Robertson <theonesuperdave@yahoo.com>
2016-08-11 14:29:02 -0500

    added sinon spy

--- KarmaWithMocha/specs/sample-sinon.spec.js
+++ KarmaWithMocha/specs/sample-sinon.spec.js
@@ -1,4 +1,4 @@
-﻿define(['sinon'], function (sinon) {
+﻿define(['sinon', 'target'], function (sinon, target) {
     describe('sample-sinonJS', function () {
         before(function () {
             // Karma creates this global __html__ property that will hold all
@@ -13,6 +13,20 @@
                 var result = sinon !== null && sinon !== undefined;
 
                 result.should.be.true();
+            });
+
+            it('sinon-spy-target', function () {
+                var callbackScoreGoal = sinon.spy(target, 'scoreGoal');
+                var callbackGoalCount = sinon.spy(target, 'goalCount');
+
+                target.scoreGoal(callbackScoreGoal);
+
+                callbackScoreGoal.calledOn(target).should.be.true();
+
+                callbackGoalCount.called.should.be.false();
+                
+                target.scoreGoal.restore();
+                target.goalCount.restore();
             });
         });
     });
