9d407900aaf5c4a018e9964cec623b3b68245e47
Dave Robertson <theonesuperdave@yahoo.com>
2017-02-10 16:47:42 -0600

    original - Inno web site installed in IIS

---
+++ InnoSetup/Inno-WebSite/install/createNewWebSiteWithAppPool.ps1
@@ -0,0 +1,32 @@
+﻿#powershell -File ".\createNewWebSiteWithAppPool.ps1" -physicalPath "C:\Program Files (x86)\web site" -appPoolName "website"
+
+param(
+    [Parameter(Mandatory=$true)][string]$physicalPath = $null,
+    [Parameter(Mandatory=$true)][string]$appPoolName = $null
+)
+
+# pre-requisites
+Import-Module WebAdministration
+
+#setup the application pool
+if(Test-Path IIS:\AppPools\$appPoolName)  
+{
+    echo "App pool exists ($appPoolName) - removing"
+    Remove-WebAppPool $appPoolName
+    gci IIS:\AppPools
+}
+$pool = New-Item IIS:\AppPools\$appPoolName
+
+#Read-Host -prompt 'hold...'
+
+if(Test-Path IIS:\Sites\$appPoolName)  
+{
+    echo "Website exists ($appPoolName) - removing"  
+    Remove-WebSite $appPoolName  
+    gci IIS:\Sites  
+}
+
+echo "Creating new website ($appPoolName)"  
+$webSite = New-Website -name $appPoolName -PhysicalPath $physicalPath -ApplicationPool $appPoolName -IPAddress "*" -Port "8081"
+
+#Read-Host -prompt 'hold...'
